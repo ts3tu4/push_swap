6125ea366ee143de4a4a8ef0e0b81d1c30663512
kikoshinozaki <c11421nm@gmail.com>
2023-05-23 00:53:16 +0900

    edit check input

--- src/check_input/check_input.c
+++ src/check_input/check_input.c
@@ -13,13 +13,30 @@
 #include "../../include/push_swap.h"
 #include "../../libft/libft.h"
 
+long int	is_intmax(char *argv)
+{
+	int		pm;
+	long	ans;
+	long	lm_div;
+
+	lm_div = LONG_MAX / 10;
+	pm = 1;
+	ans = 0;
+	if (*argv == '-' || *argv == "+")
+	{
+		if (*argv == '-')
+			pm *= -1;
+		argv ++;
+	}
+	
+}
+
 int	check_input(int argc, char **argv)
 {
 	if (argc == 1)
 		return (0);
-	if (argv > INT_MAX)//atoi変換しなきゃ
-		is_error(1);
 	if (!ft_isdigit(*argv))
 		is_error(1);
-	
+	if (is_intmax(*argv) > INT_MAX)
+		is_error(1);
 }
