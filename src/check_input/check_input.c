4b54007cff31310a22b14a948ace211ee9ef7647
mnanke <c11421nm@gmail.com>
2023-05-24 20:25:05 +0900

    tabun ok

--- src/check_input/check_input.c
+++ src/check_input/check_input.c
@@ -6,20 +6,20 @@
 /*   By: mnanke <mnanke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/08 19:38:14 by mnanke            #+#    #+#             */
-/*   Updated: 2023/05/16 18:26:40 by mnanke           ###   ########.fr       */
+/*   Updated: 2023/05/24 20:23:59 by mnanke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 #include "../../libft/libft.h"
 
-long int	is_intmax(char *argv)
+long int	is_over_intmax(char *argv)
 {
 	int		pm;
 	long	ans;
-	long	lm_div;
+	long	im_div;
 
-	lm_div = LONG_MAX / 10;
+	im_div = INT_MAX / 10;
 	pm = 1;
 	ans = 0;
 	if (*argv == '-' || *argv == "+")
@@ -28,9 +28,11 @@ long int	is_intmax(char *argv)
 			pm *= -1;
 		argv ++;
 	}
+	if (!ft_isdigit (argv))
+		return (1);
 	while (*argv && ft_isdigit(*argv))
 	{
-		if (ans > lm_div || (ans == lm_div && *argv - '0' > LONG_MAX % 10))
+		if (ans > im_div || (ans == im_div && *argv - '0' > INT_MAX % 10))
 			return (NULL);
 		ans = ans * 10 + *argv++ - '0';
 	}
@@ -39,8 +41,20 @@ long int	is_intmax(char *argv)
 
 int	check_input(int argc, char **argv)
 {
+	size_t	i;
+	size_t	j;
+	char	**splited_argv;
+
 	if (argc == 1)
 		return (0);
-	if (!is_intmax(*argv))
-		is_error(1);
+	i = 1;
+	j = 0;
+	while (!*argv[i++])
+		splited_argv[j++] = ft_split(*argv[i++], " ");
+	i = 0;
+	while (!*splited_argv[i++])
+	{
+		if (!is_over_intmax(*splited_argv[i++]))
+			put_error(1);
+	}
 }
