1b193408b8ad39091c79456d217052810c97f472
kikoshinozaki <c11421nm@gmail.com>
2023-06-29 02:54:38 +0900

    half way need fix three sort

--- src/sort/six_or_less.c
+++ src/sort/six_or_less.c
@@ -13,9 +13,24 @@
 #include "push_swap.h"
 #include "libft.h"
 
+int	get_min_index(t_node **list_a)
+{
+	int	min_index;
+
+	min_index = (*list_a)->index;
+	*list_a = (*list_a)->next;
+	while ((*list_a) != NULL)
+	{
+		if ((*list_a)->index < min_index)
+			min_index = (*list_a)->index;
+		*list_a = (*list_a)->next;
+	}
+	return (min_index);
+}
+
 void	move_min_to_b(t_node **list_a, t_node **list_b)
 {
-	while ((*list_a)->index != ここミニマム値入るようにする)
+	while ((*list_a)->index != get_min_index(*list_a))
 		ft_rotate(list_a);
 	ft_push(list_a, list_b);
 }
@@ -23,7 +38,7 @@ void	move_min_to_b(t_node **list_a, t_node **list_b)
 t_node	**ft_six_or_less(t_node **list_a, t_node **list_b, int argc)
 {
 	int	num;
-	
+
 	num = argc;
 	while (num > 3)
 		move_min_to_b(list_a, list_b);
